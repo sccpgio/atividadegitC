2813e35d2fdee67d43696ae77fdb9e33f63709a6
sccpgio <giovanna.maceratesi@etec.sp.gov.br>
2019-11-05 14:59:42 -0300

    inicio do projeto

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace gitc
+{
+    class Program
+    {
+       public static int somar(int valor1, int valor2)
+        {
+            return valor1 + valor2;
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
